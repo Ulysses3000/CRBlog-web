d74340debcc4f0302cd18fe4e068d55b31baf6ec
costin <constin@constindeMacBook-Pro.local>
2020-07-30 18:07:58 +0800

    A blog head update UI

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -4,26 +4,61 @@ import './blog-index.styl'
 
 import Bar from './bar'
 
+function BlogListHead () {
+  return (
+    <Head>
+      <title>云川·Blog</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta name="keywords" content="云川·RC, 云川, Clound River, clound river, river clound, River Clound, RC Blog, 云川博客" />
+      <link rel="icon" href="/images/logo_000.png" />
+    </Head>
+  )
+}
+
 export default class BlogList extends React.Component {
-  render () {
+
+  constructor () {
+    super()
+    this.state = {
+      searchPlaceholder: '',
+      searchValue: ''
+    }
+  }
+
+  searchChange (e) {
+    this.setState({
+      searchValue: e.target.value
+    })
+  }
+
+  renderHead () {
     return (
-      <div className="page">
-        <Head>
-          <title>云川·Blog</title>
-          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-          <meta name="keywords" content="云川·RC, 云川, Clound River, clound river, river clound, River Clound, RC Blog, 云川博客" />
-          <link rel="icon" href="/images/logo_000.png" />
-        </Head>
-        <div className="head">
+      <div className="head">
           <div className="brand">
-            <span>logo</span>
-            {/* <h1>云川博客</h1> */}
+            <img src="/images/logo_000.png" width="32" alt=""/>
+            <h1>云川博客 <a href="/blog">云川博客</a> </h1>
           </div>
-          <input type="text"/>
+          <input type="text"
+            placeholder={ this.state.searchPlaceholder }
+            value={ this.state.searchValue }
+            onChange={ (e) => { this.searchChange(e) } } />
           <div className="page-nav">
-            <div className="icon">o</div>
+            <div className="icon">
+              { Array(9).fill('').map((v,i) => {
+                return ( <span key={i} /> );
+              }) }
+            </div>
           </div>
         </div>
+        
+    )
+  }
+
+  render () {
+    return (
+      <div className="blog-list-page">
+        <BlogListHead />
+        { this.renderHead() }
         <div className="main">
           <div className="bar">
             <Bar />
